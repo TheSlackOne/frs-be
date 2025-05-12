12344a6c9163a42f0e0f801c09958e70834bc45d
Ernesto Messina <ernesto.messina@gmail.com>
2025-05-11 21:12:01 -0300

    Passing unit tests.

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,11 @@ interface Item {
     price: Number
 }
 
+interface ErrorStructure {
+    field: String,
+    message: String
+}
+
 // ToDo: In-memory database. Replace by a persistent database.
 // const inMemoryDb = new Map<number, Item>()
 let inMemoryDb: Array<Item> = []
@@ -73,7 +78,26 @@ export const defineRoutes = (server: Server) => {
         path: '/items',
         handler: async (request, h) => {
             const name = request.payload["name"];
+            if (!name) {
+                return h.response().code(400);
+            }
+            const errors: ErrorStructure[] = []
             const price = request.payload["price"];
+            if (!price) {
+                errors.push({
+                    field: "price",
+                    message: "Field \"price\" is required"
+                });
+            }
+            if (price < 0) {
+                errors.push({
+                    field: "price",
+                    message: "Field \"price\" cannot be negative"
+                });
+            }
+            if (errors.length) {
+                return h.response({errors: errors}).code(400);
+            }
             const item = {
                 id: createItemId(),
                 name: name,
@@ -91,6 +115,23 @@ export const defineRoutes = (server: Server) => {
             const item = await getItemById(id);
             if (!item) {
                 return h.response({ error: 'Item not found' }).code(404);
+            }
+            const price = request.payload["price"];
+            const errors: ErrorStructure[] = []
+            if (!price) {
+                errors.push({
+                    field: "price",
+                    message: "Field \"price\" is required"
+                });
+            }
+            if (price < 0) {
+                errors.push({
+                    field: "price",
+                    message: "Field \"price\" cannot be negative"
+                });
+            }
+            if (errors.length) {
+                return h.response({errors: errors}).code(400);
             }
             const updatedItem = await updateItemById(id, request.payload as Partial<Item>);
             return h.response(updatedItem).code(200);
